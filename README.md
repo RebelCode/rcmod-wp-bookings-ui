e9fb3d9ebc1a799c0a6f1d5dd0c22aed5fd665ec
Roman Lytvynenko <litvinenko95@gmail.com>
2018-03-28 16:54:46 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# RebelCode - WP Bookings UI Module
+
+[![Build Status](https://travis-ci.org/rebelcode/wp-bookings-ui-module.svg?branch=develop)](https://travis-ci.org/rebelcode/wp-bookings-ui-module)
+[![Code Climate](https://codeclimate.com/github/rebelcode/wp-bookings-ui-module/badges/gpa.svg)](https://codeclimate.com/github/rebelcode/wp-bookings-ui-module)
+[![Test Coverage](https://codeclimate.com/github/rebelcode/wp-bookings-ui-module/badges/coverage.svg)](https://codeclimate.com/github/rebelcode/wp-bookings-ui-module/coverage)
+[![Latest Stable Version](https://poser.pugx.org/rebelcode/wp-bookings-ui-module/version)](https://packagist.org/packages/rebelcode/wp-bookings-ui-module)
+[![Latest Unstable Version](https://poser.pugx.org/rebelcode/wp-bookings-ui-module/v/unstable)](https://packagist.org/packages/rebelcode/wp-bookings-ui-module)
+[![This package complies with Dhii standards](https://img.shields.io/badge/Dhii-Compliant-green.svg?style=flat-square)][Dhii]
+
+WP Bookings plugin's UI Module
+
+[Dhii]: https://github.com/Dhii/dhii
---
+++ test/functional/MyClassTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace RebelCode\WPBookingsUI\FuncTest;
+
+use Xpmock\TestCase;
+use RebelCode\WPBookingsUI\MyClass as TestSubject;
+
+/**
+ * Tests {@see TestSubject}.
+ *
+ * @since [*next-version*]
+ */
+class MyClassTest extends TestCase
+{
+    /**
+     * The name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'RebelCode\WPBookingsUI\MyClass';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return TestSubject
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+            ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(
+            static::TEST_SUBJECT_CLASSNAME,
+            $subject,
+            'A valid instance of the test subject could not be created.'
+        );
+    }
+}
