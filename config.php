0f3d151fcf9eb8e72fb523ec1ec982512ec57c96
Roman Lytvynenko <litvinenko95@gmail.com>
2018-03-29 11:01:16 +0300

    add registering pages, screen options and menu entries

--- config.php
+++ config.php
@@ -5,5 +5,27 @@ return [
         'id' => 'service_booking_settings',
         'post_type' => 'download',
         'title' => __('Booking Options', 'eddbk'),
+    ],
+    'menu' => [
+        'root' => [
+            'page_title' => __('Bookings', 'eddbk'),
+            'menu_title' => __('Bookings', 'eddbk'),
+            'capability' => 'publish_bookings',
+            'menu_slug' => 'eddbk-bookings',
+            'icon' => 'dashicons-calendar-alt',
+            'position' => 20
+        ],
+        'settings' => [
+            'page_title' => __('Settings', 'eddbk'),
+            'menu_title' => __('Settings', 'eddbk'),
+            'capability' => 'publish_bookings',
+            'menu_slug' => 'eddbk-settings'
+        ],
+        'about' => [
+            'page_title' => __('About', 'eddbk'),
+            'menu_title' => __('About', 'eddbk'),
+            'capability' => 'publish_bookings',
+            'menu_slug' => 'eddbk-about'
+        ]
     ]
 ];
--- src/UIModule.php
+++ src/UIModule.php
@@ -9,6 +9,8 @@ use RebelCode\Modular\Module\AbstractBaseModule;
 
 class UIModule extends AbstractBaseModule
 {
+    private $bookingsPageId;
+
     /**
      * Constructor.
      *
@@ -52,15 +54,28 @@ class UIModule extends AbstractBaseModule
         $eventManager->attach('admin_init', function () use ($c) {
             $this->_adminInit($c);
         });
+
+        $eventManager->attach('admin_menu', function () use ($c) {
+            $this->_adminMenu($c);
+        });
     }
 
     /**
      * Register hook on admin init which will register everything else.
      *
      * @param ContainerInterface $c
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
      */
     protected function _adminInit(ContainerInterface $c)
     {
+        /* @var $eventManager EventManagerInterface */
+        $eventManager = $c->get('event-manager');
+
+        /*
+         * Add metabox with availablities configuration to
+         * service's edit page.
+         */
         add_meta_box(
             $this->_getConfig()['metabox']['id'],
             $this->_getConfig()['metabox']['title'],
@@ -69,6 +84,71 @@ class UIModule extends AbstractBaseModule
             },
             $this->_getConfig()['metabox']['post_type']
         );
+
+        /*
+         * Add screen options on bookings management page.
+         */
+        $eventManager->attach('screen_settings', function($settings, \WP_Screen $screen) use ($c) {
+            if ($this->bookingsPageId !== $screen->base)
+                return $settings;
+
+            return $this->_renderBookingsScreenOptions($c);
+        });
+    }
+
+    /**
+     * Register pages in the admin menu.
+     *
+     * @param ContainerInterface $c
+     */
+    protected function _adminMenu(ContainerInterface $c)
+    {
+        $this->bookingsPageId = add_menu_page(
+            $this->_getConfig()['menu']['root']['page_title'],
+            $this->_getConfig()['menu']['root']['menu_title'],
+            $this->_getConfig()['menu']['root']['capability'],
+            $this->_getConfig()['menu']['root']['menu_slug'],
+            function () use ($c) {
+                return $this->_renderMainPage($c);
+            },
+            $this->_getConfig()['menu']['root']['icon'],
+            $this->_getConfig()['menu']['root']['position']
+        );
+
+        add_submenu_page(
+            $this->_getConfig()['menu']['root']['menu_slug'],
+            $this->_getConfig()['menu']['settings']['page_title'],
+            $this->_getConfig()['menu']['settings']['menu_title'],
+            $this->_getConfig()['menu']['settings']['capability'],
+            $this->_getConfig()['menu']['settings']['menu_slug'],
+            function () use ($c) {
+                return $this->_renderSettingsPage($c);
+            }
+        );
+
+        add_submenu_page(
+            $this->_getConfig()['menu']['root']['menu_slug'],
+            $this->_getConfig()['menu']['about']['page_title'],
+            $this->_getConfig()['menu']['about']['menu_title'],
+            $this->_getConfig()['menu']['about']['capability'],
+            $this->_getConfig()['menu']['about']['menu_slug'],
+            function () use ($c) {
+                return $this->_renderAboutPage($c);
+            }
+        );
+    }
+
+    /**
+     * Render screen options
+     *
+     * @param ContainerInterface $c
+     * @return mixed
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     */
+    protected function _renderBookingsScreenOptions(ContainerInterface $c)
+    {
+        return $c->get('template-manager')->render('booking/screen-options');
     }
 
     /**
@@ -83,5 +163,40 @@ class UIModule extends AbstractBaseModule
     protected function _renderMetabox($c)
     {
         return $c->get('template-manager')->render('availability/metabox');
+    }
+
+    /**
+     * Render main booking view.
+     *
+     * @param $c
+     * @return mixed
+     */
+    protected function _renderMainPage($c)
+    {
+        return $c->get('template-manager')->render('booking/bookings-page');
+    }
+
+    /**
+     * Render settings page.
+     *
+     * @param $c
+     * @return string
+     * @throws \Exception
+     */
+    protected function _renderSettingsPage($c)
+    {
+        throw new \Exception('Implement Settings page.');
+    }
+
+    /**
+     * Render about page.
+     *
+     * @param $c
+     * @return string
+     * @throws \Exception
+     */
+    protected function _renderAboutPage($c)
+    {
+        throw new \Exception('Implement About page.');
     }
 }
