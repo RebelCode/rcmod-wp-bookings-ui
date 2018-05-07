c3b59918d482fb67eb27e7ae8ec6babedbc842b3
Roman Lytvynenko <litvinenko95@gmail.com>
2018-05-07 17:49:25 +0300

    update caps

--- config/config.php
+++ config/config.php
@@ -15,7 +15,7 @@ return [
             'root' => [
                 'page_title' => 'Bookings',
                 'menu_title' => 'Bookings',
-                'capability' => 'publish_bookings',
+                'capability' => 'publish_posts',
                 'menu_slug' => 'eddbk-bookings',
                 'icon' => 'dashicons-calendar-alt',
                 'position' => 20
@@ -23,13 +23,13 @@ return [
             'settings' => [
                 'page_title' => 'Settings',
                 'menu_title' => 'Settings',
-                'capability' => 'publish_bookings',
+                'capability' => 'publish_posts',
                 'menu_slug' => 'eddbk-settings'
             ],
             'about' => [
                 'page_title' => 'About',
                 'menu_title' => 'About',
-                'capability' => 'publish_bookings',
+                'capability' => 'publish_posts',
                 'menu_slug' => 'eddbk-about'
             ]
         ],
